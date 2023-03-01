fa7ad44ef8fe0f68abfe02324db601cea4bc2014
Lenny <65754609+JewishLewish@users.noreply.github.com>
2023-03-01 15:54:08 -0800

    Create regional.c

---
+++ regional.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct region {
+  void *start;
+  size_t size;
+  void *current;
+} region;
+
+region *create_region(size_t size) {
+  region *r = (region *)malloc(sizeof(region));
+  r->start = malloc(size);
+  r->size = size;
+  r->current = r->start;
+  return r;
+}
+
+void *region_alloc(region *r, size_t size) {
+  if (r->current + size > r->start + r->size) {
+    return NULL; // Out of memory
+  }
+  void *p = r->current;
+  r->current += size;
+  return p;
+}
+
+void free_region(region *r) {
+    free(r->start);
+    free(r);
+}
+
+void *get_from_region(region *r, size_t offset, size_t size) {
+    if (offset < 0 || offset + size > r->size) {
+        return NULL; // Out of memory
+    }
+    return (char *)r->start + offset;
+}
+
+int test() {
+    region *r = create_region(sizeof(int)*2);
+
+    int *y = (int *)region_alloc(r, sizeof(int));
+    *y = 42;
+
+    int x;
+    memcpy(&x, get_from_region(r, (size_t)y - (size_t)r->start, sizeof(int)), sizeof(int));
+
+    free_region(r);
+}
+
+int main() {
+    region *r = create_region(sizeof(int));
+
+    int *p = (int *)region_alloc(r, sizeof(int));
+    int q = *p;
+    q = 20;
+    
+    printf("%d\n",q);
+
+    free_region(r);
+    return 0;
+}
