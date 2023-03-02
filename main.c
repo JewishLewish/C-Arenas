8ae7b387015f41341e2f57599b98283ad9f5cbaa
JewishLewish <frostyornot@gmail.com>
2023-03-01 16:35:38 -0800

    Adding Benchmarks on Memory Leaks and Freeing

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "regional.c"
+#include <time.h>
 
+//Testing if the Regional Memory Management System Works
 int test() {
     region *r = create_region(sizeof(int)*2);
 
@@ -14,7 +16,56 @@ int test() {
     printf("%d\n", x);
 }
 
+//Benchmarking
+#define NUM_VARS 1000000
+
+int Cmallocandfree() {
+    int **vars = malloc(sizeof(int*) * NUM_VARS);
+
+    clock_t start = clock();
+
+    for (int i = 0; i < NUM_VARS; i++) {
+        vars[i] = malloc(sizeof(int));
+        *vars[i] = i;
+    }
+
+    for (int i = 0; i < NUM_VARS; i++) {
+        free(vars[i]);
+    }
+
+    free(vars);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", time_spent);
+
+    return 0;
+}
+
+int Mymallocandfree() {
+    region *r = create_region(sizeof(int*) * NUM_VARS + sizeof(int) * NUM_VARS);
+
+    int **vars = (int **)region_alloc(r, sizeof(int*) * NUM_VARS);
+
+    clock_t start = clock();
+
+    for (int i = 0; i < NUM_VARS; i++) {
+        vars[i] = (int *)region_alloc(r, sizeof(int));
+        *vars[i] = i;
+    }
+
+    free_region(r);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", time_spent);
+
+    return 0;
+}
+
+
 int main() {
-    test();
+    Cmallocandfree();
+    Mymallocandfree();
     return 0;
 }
